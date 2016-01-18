b1a3cdd04006ba4951272e383fa102c1ce409f30
Uriah Galang <masterpowers001@gmail.com>
2016-01-18 07:59:25 +0000

    cache another try

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,30 +12,21 @@ use Cache;
 class ProductController extends Controller
 {
     public function show(Request $request){
-    	// $products = Product::paginate(3);
-    	// $products->setPath('/');
-    	// if ($request->ajax()) {
-     //        return Response::json(\View::make('layouts.products')->with(compact('products'))->render());
-     //    }
-    	// to use Cache Version Move the Route in Web Middleware in route.php
-        // Uncomment Code Above if You Intend to Cache
+
         if (Cache::has('products')) {
         	$products = Cache::pull('products');
-        	if ($request->ajax()) {
+        	
             return Response::json(\View::make('layouts.products')->with(compact('products'))->render());
         }
          
-        }
+        
         $products = Cache::rememberForever('products', function () {
         	$products = Product::paginate(3);
         	$products->setPath('/');
     	return $products;
-    	
-
 		});
-		if ($request->ajax()) {
+
 		$products = Cache::pull('products');
 		return Response::json(\View::make('layouts.products')->with(compact('products'))->render());
- 		}
     }
 }
