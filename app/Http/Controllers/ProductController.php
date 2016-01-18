3164b54654b876d6c1acfe55f3bc77a478e40115
Uriah Galang <masterpowers001@gmail.com>
2016-01-18 08:41:35 +0000

    revert back to no caching until further notice

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,26 +7,15 @@ use App\Product;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Response;
-use Cache;
 
 class ProductController extends Controller
 {
     public function show(Request $request){
-
-        if (Cache::has('products')) {
-        	$products = Cache::pull('products');
-        	
+    	$products = Product::paginate(3);
+    	$products->setPath('/');
+    	if ($request->ajax()) {
             return Response::json(\View::make('layouts.products')->with(compact('products'))->render());
         }
-         
-        
-        $products = Cache::rememberForever('products', function () {
-        	$products = Product::paginate(3);
-        	$products->setPath('/');
-    	return $products;
-		});
-
-		$products = Cache::pull('products');
-		return Response::json(\View::make('layouts.products')->with(compact('products'))->render());
+ 
     }
 }
