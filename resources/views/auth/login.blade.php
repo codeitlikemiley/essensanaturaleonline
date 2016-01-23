8c3bd11fa99e42d38ec6ae2a1282aab584e8e4b0
Uriah Galang <masterpowers001@gmail.com>
2016-01-23 11:44:15 +0000

    change modal to login blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,4 +40,15 @@
 <!--Import Google Recaptcha-->
     {!! Html::script('js/parsley.min.js') !!}
     @include('layouts.recaptcha')
+    
+    <script>
+    $('#tosmodal').leanModal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: '.6', // Opacity of modal background
+      in_duration: 300, // Transition in duration
+      out_duration: 200, // Transition out duration
+      ready: function() { console.log('Open'); }, // Callback for Modal open
+      complete: function() { console.log('Closed'); } // Callback for Modal close
+      });
+    </script>
 @stop
