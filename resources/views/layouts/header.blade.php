2f738c5a919419cf61dbab17e8e8ef716860cfdd
Uriah Galang <masterpowers001@gmail.com>
2016-01-31 04:59:30 +0000

    revert back to google link font

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,6 +1,6 @@
 <head>
   <!-- meta tag  -->
-   
+   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <meta name="csrf-token" content={{ csrf_token() }}/>
