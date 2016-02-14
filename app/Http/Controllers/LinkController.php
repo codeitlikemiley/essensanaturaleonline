bacb8b323290a1867c8ff03b369472fc872c17dd
Uriah Galang <masterpowers001@gmail.com>
2016-02-14 09:39:35 +0000

    If User is Not Yet Member Get Active Sponsor

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -30,14 +30,48 @@ class LinkController extends Controller
      */
      public function showRefLink($link = null)
      {
+        // if Link is not Active! refer the link of the Next Active Upline
+         if(!Link::findByLink($link)->active){ // if Link Not Active!
+            $sponsor = Link::findByLink($link);
+            $sp_lid = $sponsor->id;
+            $sp_lid= $sponsor->activeSponsor($sp_lid);
+            
+            try {
+             // If has $Link then Look in Database if Exist
+            $link  = Link::find($sp_lid)->load('user.profile');
 
+            // $splink = $link->toArray();
+            $splink = [];
+            $splink['id'] = $link->id;
+            $splink['user_id'] = $link->user_id;
+            $splink['link'] = $link->link;
+            $product = Product::find(1);
+            
+            // Note Cookie Wont Be Created if Exceeded More than 4kb
+            $cookie = \Cookie::queue('sponsor', $splink, 2628000);
+
+            // Return Referral View with Variable Link
+              return view('pages.referralLink')->with(compact('link','product'));
+
+        // If No Record Found Throw Exception!
+         } catch (ModelNotFoundException $e) {
+             // Return Back to Home
+        return Redirect::to('/');
+
+            // return view('nosponsor');
+         }
+
+            }
         //  // If it has a Sponsor Cookie
          if (\Cookie::has('sponsor')) {
             $cookie = \Cookie::get('sponsor');
             $link = $cookie['link'];
+            if(Link::findByLink($link)->active){
             $link  = Link::findByLink($link)->load('user.profile');
             $product = Product::find(1);
-             return view('pages.referralLink')->with(compact('link','product')); // Load Referral Link View
+             return view('pages.referralLink')->with(compact('link','product')); 
+            }
+            
          }
          if (is_null($link)) {
              return Redirect::to('/'); // Redirect To HomePage
@@ -46,6 +80,7 @@ class LinkController extends Controller
          try {
              // If has $Link then Look in Database if Exist
             $link  = Link::findByLink($link)->load('user.profile');
+
             // $splink = $link->toArray();
             $splink = [];
             $splink['id'] = $link->id;
