bad51d16d75570416256c7effe2812c38c98e0df
Uriah Galang <masterpowers001@gmail.com>
2016-02-15 09:23:19 +0000

    test2

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,7 +15,7 @@ class DashboardController extends Controller
 {
 	public function __construct()
     {
-        $this->middleware('member', ['only' => ['viewProfile']]);
+        $this->middleware('member', ['only' => ['showProfile']]);
         $this->middleware('auth', ['except' => ['viewProfile']]);
     }
 
