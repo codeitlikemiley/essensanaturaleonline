484641380ab34cd2c05685d452d4f3ef86615eda
Uriah Galang <masterpowers001@gmail.com>
2016-02-16 06:07:13 +0000

    refactor showreflink method

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -28,79 +28,54 @@ class LinkController extends Controller
      *
      * @return [json] [all info about the link]
      */
-     public function showRefLink($link = null)
-     {
-        // if Link is not Active! refer the link of the Next Active Upline
-         if(!Link::findByLink($link)->active){ // if Link Not Active!
-            $sponsor = Link::findByLink($link);
-            $sp_lid = $sponsor->id;
-            $sp_lid= $sponsor->activeSponsor($sp_lid);
-            
-            try {
-             // If has $Link then Look in Database if Exist
-            $link  = Link::find($sp_lid)->load('user.profile');
-
-            // $splink = $link->toArray();
-            $splink = [];
-            $splink['id'] = $link->id;
-            $splink['user_id'] = $link->user_id;
-            $splink['link'] = $link->link;
-            $product = Product::find(1);
-            
-            // Note Cookie Wont Be Created if Exceeded More than 4kb
-            $cookie = \Cookie::queue('sponsor', $splink, 2628000);
-
-            // Return Referral View with Variable Link
-              return view('pages.referralLink')->with(compact('link','product'));
-
-        // If No Record Found Throw Exception!
-         } catch (ModelNotFoundException $e) {
-             // Return Back to Home
-        return Redirect::to('/');
-
-            // return view('nosponsor');
-         }
-
-            }
+    public function showRefLink($link = null)
+    {
+        // Link is NULL 
+        if (is_null($link)) {
+            return Redirect::to('/');
+        }
         //  // If it has a Sponsor Cookie
-         if (\Cookie::has('sponsor')) {
+        if (\Cookie::has('sponsor')) {
             $cookie = \Cookie::get('sponsor');
             $link = $cookie['link'];
-            if(Link::findByLink($link)->active){
             $link  = Link::findByLink($link)->load('user.profile');
             $product = Product::find(1);
-             return view('pages.referralLink')->with(compact('link','product')); 
+            return view('pages.referralLink')->with(compact('link','product'));
+        }
+        // Check if the Provided Link is Valid  Redirect to Home if Invalid Link!
+        try {
+            $link = Link::findByLink($link);
+            $sp_lid = $link->id;
+            // If Sponsor is Not Active LeapFrog to ActiveSponsor
+            if(!$link->active){
+            $sp_lid= $link->activeSponsor($sp_lid);
             }
-            
-         }
-         if (is_null($link)) {
-             return Redirect::to('/'); // Redirect To HomePage
-         }
-
-         try {
-             // If has $Link then Look in Database if Exist
-            $link  = Link::findByLink($link)->load('user.profile');
-
-            // $splink = $link->toArray();
+            // Load User Profile
+            $link  = Link::find($sp_lid)->load('user.profile');
             $splink = [];
             $splink['id'] = $link->id;
             $splink['user_id'] = $link->user_id;
             $splink['link'] = $link->link;
+            // Load Product
             $product = Product::find(1);
             
+            // Assign $splink to the cookie
             // Note Cookie Wont Be Created if Exceeded More than 4kb
+            // Cookie set Forever / 5 Years or until Cache Clear
+            // No Needed To Return With Cookie if it is Queue
+            
             $cookie = \Cookie::queue('sponsor', $splink, 2628000);
 
             // Return Referral View with Variable Link
               return view('pages.referralLink')->with(compact('link','product'));
 
-        // If No Record Found Throw Exception!
-         } catch (ModelNotFoundException $e) {
-             // Return Back to Home
-        return Redirect::to('/');
+            // LINK PROVIDED IS INVALID REDIRECT HOME
+            } catch (ModelNotFoundException $e) {
+                return Redirect::to('/');
 
-            // return view('nosponsor');
          }
-     }
+    }
+
+         
 
 }
