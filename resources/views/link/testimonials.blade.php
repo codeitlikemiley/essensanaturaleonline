a1d32633463de65bf7468731c9e044aebf82d9ea
Uriah Galang <masterpowers001@gmail.com>
2016-02-25 05:09:02 +0000

    remove big image

--- resources/views/link/testimonials.blade.php
+++ resources/views/link/testimonials.blade.php
@@ -1,31 +1,28 @@
 <div class="row center">
 <h5 class="amber-text">Users Testimonials</h5>
-<div class="col l6 m6 s12 hide-on-large-only left">
+<div class="col l6 m6 s12  left">
 <img class="materialboxed responsive-img" data-caption="Testimonials From Our Users" src="{{ url('/') }}/img/testi1.jpg">
 </div>
-<div class="col l6 m6 s12 hide-on-large-only right">
+<div class="col l6 m6 s12  right">
 <img class="materialboxed responsive-img" data-caption="Testimonials From Our Users" src="{{ url('/') }}/img/testi2.jpg">
 </div>
-<div class="col l6 m6 s12 hide-on-large-only left">
+<div class="col l6 m6 s12  left">
 <img class="materialboxed responsive-img" data-caption="Testimonials From Our Users" src="{{ url('/') }}/img/testi3.jpg">
 </div>
-<div class="col l6 m6 s12 hide-on-large-only right">
+<div class="col l6 m6 s12  right">
 <img class="materialboxed responsive-img" data-caption="Testimonials From Our Users" src="{{ url('/') }}/img/testi4.jpg">
 </div>
-<div class="col l6 m6 s12 hide-on-large-only left">
+<div class="col l6 m6 s12  left">
 <img class="materialboxed responsive-img" data-caption="Testimonials From Our Users" src="{{ url('/') }}/img/testi5.jpg">
 </div>
-<div class="col l6 m6 s12 hide-on-large-only right">
+<div class="col l6 m6 s12  right">
 <img class="materialboxed responsive-img" data-caption="Testimonials From Our Users" src="{{ url('/') }}/img/testi6.jpg">
 </div>
-<div class="col l6 m6 s12 hide-on-large-only left">
+<div class="col l6 m6 s12  left">
 <img class="materialboxed responsive-img" data-caption="Testimonials From Our Users" src="{{ url('/') }}/img/testi7.jpg">
 </div>
-<div class="col l6 m6 s12 hide-on-large-only right">
+<div class="col l6 m6 s12  right">
 <img class="materialboxed responsive-img" data-caption="Testimonials From Our Users" src="{{ url('/') }}/img/testi8.jpg">
-</div>
-<div class="col s12">
-<img class="materialboxed responsive-img hide-on-med-and-down" data-caption="Testimonials From Our Users" src="{{ url('/') }}/img/buahmerah-testimonials.jpg">
 </div>
 
 </div>
