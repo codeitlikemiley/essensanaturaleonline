643a586cba1c69f65c3b8a4faf9713b975690176
Uriah Galang <masterpowers001@gmail.com>
2016-02-25 22:20:40 +0000

    change layout m8

--- resources/views/pages/referralLink.blade.php
+++ resources/views/pages/referralLink.blade.php
@@ -11,7 +11,7 @@
 <!-- START Main Content -->
 <main>
   <div class="row">
-    <div class="col l9 m9 s12">
+    <div class="col l9 m8 s12">
       <ul class="tabs light-blue lighten-5 z-depth-2">
         <li class="tab col s3" style="min-width: 120px;"><a class="active" href="#tab1">Buah Merah</a></li>
         <li class="tab col s3" style="min-width: 120px;"><a href="#tab2">Company</a></li>
@@ -22,28 +22,28 @@
         <li class="tab col s3" style="min-width: 120px;"><a href="#tab7">How To Order</a></li>
       </ul>
     </div>
-    <div id="tab1" class="col l9 m9 s12"> 
+    <div id="tab1" class="col l9 m8 s12"> 
     @include('link.buahmerah')
     </div>
-    <div id="tab2" class="col l9 m9 s12">
+    <div id="tab2" class="col l9 m8 s12">
     @include('link.company')
     </div>
-    <div id="tab3" class="col l9 m9 s12">
+    <div id="tab3" class="col l9 m8 s12">
     @include('link.legalities')
     </div>
-    <div id="tab4" class="col l9 m9 s12">
+    <div id="tab4" class="col l9 m8 s12">
     @include('link.awards')
     </div>
-    <div id="tab5" class="col l9 m9 s12">
+    <div id="tab5" class="col l9 m8 s12">
     @include('link.testimonials')
     </div>
-    <div id="tab6" class="col l9 m9 s12">
+    <div id="tab6" class="col l9 m8 s12">
     @include('link.howToDrink')
     </div>
-    <div id="tab7" class="col l9 m9 s12">
+    <div id="tab7" class="col l9 m8 s12">
     @include('link.howToOrder')
     </div>
-    <div class="col l3 m3 s12 right">
+    <div class="col l3 m4 s12 right">
     @include('layouts.loading')
     @include('layouts.forms.addToCartBuahMerah')
     @include('layouts.forms.contactMe')
