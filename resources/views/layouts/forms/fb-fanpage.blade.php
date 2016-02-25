4638fe285959f29c96e90016e20d9e4145176099
Uriah Galang <masterpowers001@gmail.com>
2016-02-25 20:51:12 +0000

    resize fb fanpage width

--- resources/views/layouts/forms/fb-fanpage.blade.php
+++ resources/views/layouts/forms/fb-fanpage.blade.php
@@ -1,5 +1,6 @@
 <div id="fb-root"></div>
-<div data-lazy-widget="fb_1" class="fb-page collapsible-body" data-href="{{ $link->user->profile->social_links['fb-fanpage'] }}" data-tabs="messages" data-height="350" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"></div>
+<div style="width:239px;">
+<div data-lazy-widget="fb_1" class="fb-page collapsible-body" data-href="{{ $link->user->profile->social_links['fb-fanpage'] }}" data-tabs="messages" data-width="300" data-height="350" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"></div>
 <div id="fb_1"><!--
     <script>(function(d, s, id) {
 	  var js, fjs = d.getElementsByTagName(s)[0];
@@ -9,3 +10,4 @@
 	  fjs.parentNode.insertBefore(js, fjs);
 	  }(document, 'script', 'facebook-jssdk'));</script>
     --></div>
+</div>
