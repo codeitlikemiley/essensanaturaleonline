ac615599f1b644cac75fc5d47125ead55ff91f82
Uriah Galang <masterpowers001@gmail.com>
2016-02-25 21:53:32 +0000

    last

--- resources/views/layouts/forms/fb-fanpage.blade.php
+++ resources/views/layouts/forms/fb-fanpage.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
 <div id="fb-root"></div>
-<div class ="col s12">
+<div style="width: 239px;">
 <div data-lazy-widget="fb_1" class="fb-page collapsible-body" data-href="{{ $link->user->profile->social_links['fb-fanpage'] }}" data-tabs="messages" data-width="314" data-height="350" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"></div>
 </div>
 <div id="fb_1"><!--
