252831cf3c0f7dfed51e0d22e6600093135ff85c
Uriah Galang <masterpowers001@gmail.com>
2016-02-26 11:02:59 +0000

    one css

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,7 +4,6 @@
 <script>
 var cb = function() {
   var sheets = [
-    'https://fonts.googleapis.com/icon?family=Material+Icons',
     'css/vendor.css'];
   
   var h = document.getElementsByTagName('head')[0]; 
@@ -23,7 +22,27 @@ webkitRequestAnimationFrame || msRequestAnimationFrame;
 if (raf) raf(cb);
 else window.addEventListener('load', cb);
 </script>
-
+<style>
+    @font-face {
+     font-family: 'Material Icons';
+     src: local('Material Icons'), url('../font/material-design-icons/Material-Design-Icons.woff2') format('woff2');
+   }
+   .material-icons {
+  font-family: 'Material Icons';
+  font-weight: normal;
+  font-style: normal;
+  font-size: 24px;
+  line-height: 1;
+  letter-spacing: normal;
+  text-transform: none;
+  display: inline-block;
+  white-space: nowrap;
+  word-wrap: normal;
+  direction: ltr;
+  -webkit-font-feature-settings: 'liga';
+  -webkit-font-smoothing: antialiased;
+}
+  </style>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <meta name="csrf-token" content={{ csrf_token() }}/>
