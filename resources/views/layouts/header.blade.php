77f54cb1785127bc1f9505c85661a5bca09e5542
Uriah Galang <masterpowers001@gmail.com>
2016-02-26 10:26:05 +0000

    test

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,6 +1,7 @@
 <head>
   <!-- meta tag  -->
-  <!-- <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link type="text/css" rel="stylesheet" href="css/vendor.css"  media="screen,projection"/>
   
 
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
--- resources/views/reflinks.blade.php
+++ resources/views/reflinks.blade.php
@@ -24,42 +24,6 @@
 
      <!--Import all JS Usable in All Page-->
      <script async src="/js/vendor.js"></script>
-     <script>
-(function() {
-    var link = document.createElement('link');
-    link.rel = "stylesheet";
-    link.href = "https://fonts.googleapis.com/icon?family=Material+Icons";
-    document.querySelector("head").appendChild(link);
-})();
-</script>
-     <script>
-      /*!
-      loadCSS: load a CSS file asynchronously.
-      */
-      function loadCSS(href){
-        var ss = window.document.createElement('link'),
-            ref = window.document.getElementsByTagName('head')[0];
-
-        ss.rel = 'stylesheet';
-        ss.href = href;
-
-        // temporarily, set media to something non-matching to ensure it'll
-        // fetch without blocking render
-        ss.media = 'only x';
-
-        ref.parentNode.insertBefore(ss, ref);
-
-        setTimeout( function(){
-          // set media back to `all` so that the stylesheet applies once it loads
-          ss.media = 'all';
-        },0);
-      }
-      loadCss('/css/vendor.css');
-    </script>
-    <noscript>
-      <!-- Let's not assume anything -->
-      <link rel="stylesheet" href="/css/vendor.css">
-    </noscript>
      <script async src="js/search.js"></script>
     <!--Import Initialize Js Components-->
     @include('layouts.jquery')
