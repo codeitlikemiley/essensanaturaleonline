b7fe588d131e03db447f6f3cf1c437e679c89049
Uriah Galang <masterpowers001@gmail.com>
2016-02-26 14:49:17 +0000

    remove nocache in middleware

--- app/Http/Middleware/ActiveMember.php
+++ app/Http/Middleware/ActiveMember.php
@@ -26,17 +26,9 @@ class ActiveMember
             }
         
             if (\Auth::user()->links->first()->active) {
-            return $this->nocache( $next($request) );
+            return $next($request);
             }
             return redirect()->action('DashboardController@showShippingAddress');
     }
 
-    protected function nocache($response)
-    {
-        $response->headers->set('Cache-Control','nocache, no-store, max-age=0, must-revalidate');
-        $response->headers->set('Expires','Wed, 17 Aug 1988 00:00:00 GMT');
-        $response->headers->set('Pragma','no-cache');
-
-        return $response;
-    }
 }
--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -31,15 +31,7 @@ class Authenticate
                 ->with( 'date', \Auth::user()->created_at);
         }
 
-        return $this->nocache( $next($request) );
+        return $next($request);
     }
 
-    protected function nocache($response)
-    {
-        $response->headers->set('Cache-Control','nocache, no-store, max-age=0, must-revalidate');
-        $response->headers->set('Expires','Wed, 17 Aug 1988 00:00:00 GMT');
-        $response->headers->set('Pragma','no-cache');
-
-        return $response;
-    }
 }
