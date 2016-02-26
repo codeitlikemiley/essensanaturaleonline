697054e1949816f4d66765888f105f75bdf791a0
Uriah Galang <masterpowers001@gmail.com>
2016-02-26 14:34:39 +0000

    remove async

--- resources/views/reflinks.blade.php
+++ resources/views/reflinks.blade.php
@@ -23,7 +23,7 @@
     @include('layouts.navbutton')
 
      <!--Import all JS Usable in All Page-->
-     <script async src="/js/vendor.js"></script>
+     <script  src="/js/vendor.js"></script>
      <script  src="/js/search.js"></script>
     <!--Import Initialize Js Components-->
     @include('layouts.jquery')
