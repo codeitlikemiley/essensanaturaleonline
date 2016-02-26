93926bfebbac154f0b8f1ba2182568981ef01f3a
Uriah Galang <masterpowers001@gmail.com>
2016-02-26 09:48:51 +0000

    load async css

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,9 +1,5 @@
 <head>
   <!-- meta tag  -->
-  <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-      <!--Import materialize.css-->
-  <link type="text/css" rel="stylesheet" href="css/vendor.css"  media="screen,projection"/>
   <script>
       /*!
       loadCSS: load a CSS file asynchronously.
@@ -27,10 +23,12 @@
         },0);
       }
       loadCss('css/vendor.css');
+      loadCss('https://fonts.googleapis.com/icon?family=Material+Icons')
     </script>
     <noscript>
       <!-- Let's not assume anything -->
-      <link rel="stylesheet" href="css/vendor.css">
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link type="text/css" rel="stylesheet" href="css/vendor.css"  media="screen,projection"/>
     </noscript>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
