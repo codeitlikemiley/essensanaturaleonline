1cc732911b30336d947317a8282766cd4d4d74f9
Uriah Galang <masterpowers001@gmail.com>
2016-02-27 10:37:54 +0000

    disable middleware httpcache

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-        \Barryvdh\HttpCache\Middleware\CacheRequests::class,
+        // \Barryvdh\HttpCache\Middleware\CacheRequests::class,
     ];
 
     /**
