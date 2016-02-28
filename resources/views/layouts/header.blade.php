e55e0816facc2fb33e34f74b56e2a2a74029dfa6
Uriah Galang <masterpowers001@gmail.com>
2016-02-28 13:49:22 +0000

    remove;

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -25,7 +25,7 @@
   -webkit-font-smoothing: antialiased;
 }
   </style>
-  <link type="text/css" rel="stylesheet" href="{{ asset('css/vendor.css'); }}"  media="screen,projection"/>
+  <link type="text/css" rel="stylesheet" href="{{ asset('css/vendor.css') }}"  media="screen,projection"/>
   
 
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
