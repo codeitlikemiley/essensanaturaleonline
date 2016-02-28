3c452ffd8e9283bd198d89112e42fffbc5a0e7c7
Uriah Galang <masterpowers001@gmail.com>
2016-02-28 08:55:57 +0000

    fix message from

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -14,7 +14,7 @@ class MailController extends Controller
     public function send($user, $template, $data)
     {
         \Mail::send($template, $data, function ($message) use ($user, $data) {
-            $message->from(config('mail.from.address'));
+            $message->from();
             $message->subject($data['subject']);
             $message->to($user->email, $user->username);
         });
